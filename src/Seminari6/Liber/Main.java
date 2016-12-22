d3234c70cc05548da65bac7639b98b2987467579
Silvi Milaj <silviwdev@gmail.com>
2016-12-22 18:06:21 +0100

    Seminari 6 (Liber): Klasa Main (Marrja e te dhenave nga perdoruesi)

--- src/Seminari6/Liber/Main.java
+++ src/Seminari6/Liber/Main.java
@@ -1,14 +1,47 @@
 package Seminari6.Liber;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        Autor autor = new Autor("IDasdsfs", "Emer", "Mbiemer", "emer_mbiemer@email.com");
+        Scanner in = new Scanner(System.in);
 
-        Botuesi botuesi = new Botuesi("ID321442sa", "Shtepia Botuese", "Rr. e Shtepise Botuese");
+        String autor_emer, autor_mbiemer, autor_email,
+            botuesi_emer, botuesi_adresa,
+            liber_titulli, liber_zhanri;
+        double liber_cmimi;
 
-        Liber liber = new Liber("ISBN1231asdas", "Titulli", "Zhanri", autor, botuesi, 12.4);
+        System.out.print("Vendosni emrin e autorit: ");
+        autor_emer = in.nextLine();
+
+        System.out.print("Vendosni mbiemrin e autorit: ");
+        autor_mbiemer = in.nextLine();
+
+        System.out.print("Vendosni emailin e autorit: ");
+        autor_email = in.nextLine();
+
+        System.out.print("Vendosni emrin e botuesit: ");
+        botuesi_emer = in.nextLine();
+
+        System.out.print("Vendosni adresen e botuesit: ");
+        botuesi_adresa = in.nextLine();
+
+        System.out.print("Vendosni titullin e librit: ");
+        liber_titulli = in.nextLine();
+
+        System.out.print("Vendosni zhanrin e librit: ");
+        liber_zhanri = in.nextLine();
+
+        System.out.print("Vendosni cmimin e librit: ");
+        liber_cmimi = in.nextDouble();
+
+        Autor autor = new Autor("IDasdsfs", autor_emer, autor_mbiemer, autor_email);
+
+        Botuesi botuesi = new Botuesi("ID321442sa", botuesi_emer, botuesi_adresa);
+
+        Liber liber = new Liber("ISBN1231asdas", liber_titulli, liber_zhanri, autor, botuesi, liber_cmimi);
 
         System.out.println(
             "Titulli i Librit: "+ liber.getTitulli() + "\n"
