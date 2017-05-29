2cd3d8088fb1df02636c82b8ffef783c90177376
silvi <silviwdev@gmail.com>
2017-05-29 22:31:21 +0200

    Laboratori 5, Ushtrimi 2

--- src/Laboratori5/Ushtrimi2/Main.java
+++ src/Laboratori5/Ushtrimi2/Main.java
@@ -4,21 +4,38 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
-//        FileReader reader;
-//        try {
-//            reader = new FileReader("src/Laboratori5/Ushtrimi2/studentet.txt");
-//            FileWriter out = new FileWriter("src/Laboratori5/Ushtrimi2/out.txt");
-//
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Skedari nuk u gjet");
-//        } catch (IOException e) {
-//            System.out.println("Output file not found");
-////            reader.close();
-//        }
+        FileReader reader;
+        try {
+            reader = new FileReader("src/Laboratori5/Ushtrimi2/studentet.txt");
+            FileWriter out = new FileWriter("src/Laboratori5/Ushtrimi2/out.txt");
+
+            Scanner in = new Scanner(reader);
+            int counter = 1;
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+
+                if ( ! line.equals("") ) {
+                    String to_append = line.split("=")[1].trim() + " ";
+                    out.append(to_append);
+                    if (++counter > 3) {
+                        counter = 1;
+                        out.append("\n");
+                    }
+                }
+            }
+
+            reader.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Skedari nuk u gjet");
+        } catch (IOException e) {
+            System.out.println("Output file not found");
+        }
     }
 }
